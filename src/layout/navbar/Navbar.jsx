1542c6f1e91779a74336cf624727db9b952b6694
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-07-10 13:58:01 -0300

    navbar in process

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,7 +1,91 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import "./navbar.css";
+const navigation = [
+  { name: "INICIO", href: "#", current: false },
+  { name: "SERVICIOS", href: "#", current: false },
+  { name: "PORTFOLIO", href: "#", current: false },
+];
 
-const Navbar = () => {
-  return <nav className="navbar"></nav>;
-};
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <>
+      <Disclosure as="nav">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 nav__container">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-custom-black hover:bg-gray-700 hover:text-custom-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-800">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon
+                  aria-hidden="true"
+                  className="block h-6 w-6 group-data-[open]:hidden"
+                />
+                <XMarkIcon
+                  aria-hidden="true"
+                  className="hidden h-6 w-6 group-data-[open]:block"
+                />
+              </DisclosureButton>
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-1 items-center">
+                <img
+                  alt="Imprenta Universo"
+                  src="/img/logo universo svg.svg"
+                  className="h-12 w-auto"
+                />
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? "page" : undefined}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-900 text-white"
+                          : "text-black hover:bg-gray-900 hover:text-white",
+                        "rounded-md mt-2 px-3 py-2 text-sm font-medium"
+                      )}>
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <DisclosurePanel className="sm:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <DisclosureButton
+                key={item.name}
+                as="a"
+                href={item.href}
+                aria-current={item.current ? "page" : undefined}
+                className={classNames(
+                  item.current
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium "
+                )}>
+                {item.name}
+              </DisclosureButton>
+            ))}
+          </div>
+        </DisclosurePanel>
+      </Disclosure>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-bgOrange": "#ffce8f",
+        "custom-yellow": "#fef200",
+        "custom-black": "#231f20",
+        "custom-light-blue": "#0aabea",
+        "custom-pink": "#ed008c",
+        "custom-gray": "#a8a9ad",
+        "custom-green": "#014b38",
+        "custom-white": "#ffffff",
+      },
+    },
   },
   plugins: [],
-}
-
+};
