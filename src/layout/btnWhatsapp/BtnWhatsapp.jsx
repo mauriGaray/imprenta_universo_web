9dd29b0df2f2d7f8ae7b8299b60f8393a419fa09
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-07-15 22:03:57 -0300

    btn whatsapp fixed

--- src/layout/btnWhatsapp/BtnWhatsapp.jsx
+++ src/layout/btnWhatsapp/BtnWhatsapp.jsx
@@ -2,12 +2,12 @@ import "./btnWhatsapp.css";
 
 const BtnWhatsapp = () => {
   return (
-    <div>
+    <div className="btnWhatsapp__img ">
       <a href="https://wa.me/595981482599?text=Hola!%20Me%20gustar%C3%ADa%20pedir%20una%20cotizaci%C3%B3n.">
         <img
           src="/img/btn-whatsapp.png"
           alt="botón de Whatsapp"
-          className="fixed z-50 bottom-4 right-4  px-4 py-2 flex items-center btnWhatsapp__img "
+          className="fixed z-50 bottom-4 right-4  "
         />
       </a>
     </div>
