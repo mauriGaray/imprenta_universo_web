fedecc34e8e16626d3d446d5de82a5321ed91b65
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-08-06 13:30:59 -0300

    page 404 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./layout/Layout";
 import LandingPage from "./pages/landingPage/LandingPage";
 import Portfolio from "./pages/portfolio/Portfolio";
 import { Ubicacion } from "./pages/ubicacion/Ubicacion";
+import NotFound404 from "./pages/notFound404/NotFound404";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route index path="/ubicacion" element={<Ubicacion />} />
             <Route index path="/portfolio" element={<Portfolio />} />
             <Route index path="/contacto" element={<LandingPage />} />
+            <Route index path="*" element={<NotFound404 />} />
           </Route>
         </Routes>
       </Router>
